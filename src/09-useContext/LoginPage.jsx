6dcdc6104b16160c9d7ca5f08172554a2b0e2f0b
Will_09 <wbahoque71@gmail.com>
2022-11-30 17:37:16 -0500

    Se modifica el LoginPage con el Usecontex

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
 
 
 
 export const LoginPage = () => {
+
+    const anything = useContext( UserContext );
+    console.log(anything);
+
     return (
         <>
 
